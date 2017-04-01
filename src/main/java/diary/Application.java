9724235bc1621b8e0f29f2360693b4df2cce2b0b
Ryoko <ryoko0425@gmail.com>
2017-04-01 18:06:27 +0900

    initial commit

---
+++ src/main/java/diary/Application.java
@@ -0,0 +1,16 @@
+package diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ryoko on 17/04/01.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
