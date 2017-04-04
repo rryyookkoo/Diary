b46a1a1c2ee0b39d2fb801b7ed508fa0016ebd0b
Ryoko <ryoko0425@gmail.com>
2017-04-04 13:36:36 +0900

    ・日記一覧の作成日のフォーマットを変更 ・indexControllerにて認証情報を取得し画面にユーザIDを表示する ・ログインユーザの日記一覧を取得する ・新着日記のリストを１０件までにする ・Diaryテーブルにサンプルデータを追加

--- src/main/java/diary/web/IndexController.java
+++ src/main/java/diary/web/IndexController.java
@@ -3,6 +3,8 @@ package diary.web;
 import diary.domain.Diary;
 import diary.service.IndexService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +22,30 @@ public class IndexController {
     @RequestMapping("/")
     public String index(Model model){
 
+        //認証情報の取得
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String userId;
+
+        if (principal instanceof UserDetails) {
+            userId = ((UserDetails)principal).getUsername();
+        } else {
+            userId = principal.toString();
+        }
+
+        model.addAttribute("userId", userId + "さんの日記");
+
+        //新着の日記一覧の取得
         List<Diary> publicDiary = indexService.findByIsPublicOrderByCreatedAtDesc(true);
+
+        for (int i = 9 ; i < publicDiary.size() ; i++){
+            publicDiary.remove(i);
+        }
+
         model.addAttribute("public_diaries", publicDiary);
 
-        List<Diary> userDiary = indexService.findByUserIdOrderByCreatedAtDesc("user01");
+        //ログインユーザの日記一覧を取得
+        List<Diary> userDiary = indexService.findByUserIdOrderByCreatedAtDesc(userId);
         model.addAttribute("user_diaries", userDiary);
 
         return "index";
