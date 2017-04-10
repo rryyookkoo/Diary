e0c48f592cd45c5bc4816a50c6d67bc711977470
Ryoko <ryoko0425@gmail.com>
2017-04-10 23:11:41 +0900

    会員登録画面のhtml、Form、Controllerを作成・修正（画面表示まで可能）

--- src/main/java/diary/web/AccountCreateController.java
+++ src/main/java/diary/web/AccountCreateController.java
@@ -1,6 +1,9 @@
 package diary.web;
 
+import diary.web.model.AccountCreateForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,7 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AccountCreateController {
     @RequestMapping("/account/create")
-    public String accountCreate(){
+    public String accountCreate(@ModelAttribute AccountCreateForm diaryCreateForm, Model model){
         return "account_create";
+    }
+    @RequestMapping("/account/do_create")
+    public String accountDoCreate(@ModelAttribute AccountCreateForm diaryCreateForm, Model model){
+        return "login";
     }
 }
---
+++ src/main/java/diary/web/model/AccountCreateForm.java
@@ -0,0 +1,34 @@
+package diary.web.model;
+
+/**
+ * Created by ryoko on 17/04/10.
+ */
+public class AccountCreateForm {
+    String userId;
+    String password;
+    String password2;
+
+    public String getPassword2() {
+        return password2;
+    }
+
+    public void setPassword2(String password2) {
+        this.password2 = password2;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
